b6d63d5e6730b8d594b5c4f31bdcbad6002499a2
Spartak <spartaklysman@gmail.com>
2025-11-20 22:46:43 +0100

    Testing implemented: AuthIntegrationTest and PatientIntegrationTest

---
+++ integration-tests/src/test/java/PatientIntegrationTest.java
@@ -0,0 +1,47 @@
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class PatientIntegrationTest {
+
+    @BeforeAll
+    static void setUp() {
+        RestAssured.baseURI = "http://localhost:4004";
+    }
+
+    // 1. Arrange
+    // 2. Act
+    // 3. Asset
+
+    @Test
+    public void shouldReturnPatientWithValidToken() {
+        String loginPayload = """
+                  {
+                    "email": "testuser@test.com",
+                    "password": "password123"
+                  }
+                """;
+
+        String token = given()
+                .contentType("application/json")
+                .body(loginPayload)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("token");
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/api/v1/patients")
+                .then()
+                .statusCode(200)
+                .body("patients", notNullValue());
+    }
+}
